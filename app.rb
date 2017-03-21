2133fa76c2cfc9ac72bc744c8fc8f7deaae05483
Jose A. Campos <jjoseccampos@gmail.com>
2017-03-21 18:05:07 -0400

    adventure mode

--- app.rb
+++ app.rb
@@ -1,15 +1,21 @@
 random_num = rand(1..100)
 num_guesses = 0
+guesses = []
 
 loop do
   print "Guess a number 1 to 100: "
-  guess = gets.to_i
+  guesses << gets.to_i
   num_guesses += 1
 
-  if guess == random_num
+  if guesses[-2] == guesses[-1]
+    puts "Are you doing Ok over there buddy?"
+  break
+    end
+
+  if guesses.include? random_num
     puts "You win"
     break
-  elsif guess < random_num
+  elsif guesses.last < random_num
     puts "Your guess is smaller that the number"
   else
     puts "Your guess is larger than the number"
