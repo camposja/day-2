a3ae4339273c82751dc19fa7aef83c784c57e96c
Jose A. Campos <jjoseccampos@gmail.com>
2017-03-21 15:17:09 -0400

    random number guess

--- app.rb
+++ app.rb
@@ -5,3 +5,12 @@ loop do
   print "Guess a number 1 to 100: "
   guess = gets.to_i
   num_guesses +=1
+
+  if guess == random_num
+    puts "You win"
+  elsif guess < random_num
+    puts "Your guess is smaller that the number"
+  else
+    puts "Your guess is larger than the number"
+  end
+end
